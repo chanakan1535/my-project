e0c49e3f446c041a72320a6e84682bd70ec98b5a
chanakan1535 <95395394+chanakan1535@users.noreply.github.com>
2023-10-25 13:39:08 +0700

    firebase web login

--- user_line/pages/login/app.js
+++ user_line/pages/login/app.js
@@ -1,22 +1,93 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-app.js";
-        const firebaseConfig = {
-            apiKey: "AIzaSyCobEdZH6BcltorXTDs9u1PB0cfSTGBMZ0",
-            authDomain: "fir-company-5f694.firebaseapp.com",
-            projectId: "fir-company-5f694",
-            storageBucket: "fir-company-5f694.appspot.com",
-            messagingSenderId: "1067848697539",
-            appId: "1:1067848697539:web:bf5b5c0e22430635ab4476",
-            measurementId: "G-HGN524L90P"
-        };
+import { getDatabase, set, ref, update } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-database.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut} from "https://www.gstatic.com/firebasejs/10.5.0/firebase-auth.js";
+    const firebaseConfig = {
+        apiKey: "AIzaSyCyrNBpSnag4W2D8y5qKA_miHZ3vsyE784",
+        authDomain: "authentication-app-79f47.firebaseapp.com",
+        databaseURL: "https://authentication-app-79f47-default-rtdb.firebaseio.com",
+        projectId: "authentication-app-79f47",
+        storageBucket: "authentication-app-79f47.appspot.com",
+        messagingSenderId: "155210703093",
+        appId: "1:155210703093:web:53d22d76c4689a7a7665bc"
+    };
+    const app = initializeApp(firebaseConfig);
+    const database = getDatabase(app);
+    const auth = getAuth();
+    
+    // ฟังก์ชันในการสร้างบัญชีผู้ใช้
+    // signUp.addEventListener('click', (e) => {
 
-        const app = initializeApp(firebaseConfig);
-        const form = document.getElementById("regetterform");
-const formarea = document.getElementById("form-area");
-console.log(form);
+    //     var email = document.getElementById('email').value;
+    //     var password = document.getElementById('password').value;
+    //     // var username = document.getElementById('username').value;
+    //     console.log(password);
+    //     createUserWithEmailAndPassword(auth, email, password)
+    //     .then((userCredential) => {
+    //         // Signed up 
+    //         const user = userCredential.user;
+    //         // console.log('user created');
+    //         // ...
+        
+    //         // save sign up datails in realtime database บันทึกข้อมูลในการลงทะเบียนฐานข้อมูลเรียลไทม์
+    //         set(ref(database, 'users/' + user.uid),{
+    //             email: email
+    //         })
+    //     })
+    //     .catch((error) => {
+    //         const errorCode = error.code;
+    //         const errorMessage = error.message;
+            
+    //         // console.log(errorMessage);
+    //     });
+    // });
 
-        form.addEventListener("submit",(e)=>{
-            e.preventDefault();
-            console.log(form.email.value)
-            console.log(form.password.value)
-        })
+    // ฟังก์ชันในส่วนการ login
+    login.addEventListener('click', (e) => {
+        var email = document.getElementById('email').value;
+        var password = document.getElementById('password').value;
+        
+            signInWithEmailAndPassword(auth, email, password)
+                .then((userCredential) => {
+                // Signed in 
+                const user = userCredential.user;
+                // ...
 
+                // update user login details in realtime database อัปเดตรายละเอียดการเข้าสู่ระบบของผู้ใช้ในฐานข้อมูลเรียลไทม์
+                
+                    set(ref(database, 'users/' + user.uid), {
+                        email: email
+                    });
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+            });
+    });
+
+// ฟังก์ชันในส่วนการ logout
+    const user = auth.currentUser;
+    onAuthStateChanged(auth, (user) => {
+        if (user) {
+            // User is signed in, see docs for a list of available properties
+            // https://firebase.google.com/docs/reference/js/auth.user
+            const uid = user.uid;
+            // ...
+        } else {
+            // User is signed out
+            // ...
+        }
+    });
+    logout.addEventListener('click', (e) => {
+        signOut(auth).then(() => {
+        // Sign-out successful.
+        }).catch((error) => {
+            // An error happened.
+            const errorCode = error.code;
+            const errorMessage = error.message;
+        });
+    });
+
+
+
+
+         
